4ab276aa496616543174aed79dc1bd130fd35907
alindafortunate <alindafortunate5@gmail.com>
2025-01-16 12:50:25 +0300

    Used the read() function.

--- using_open_and_read_functions.py
+++ using_open_and_read_functions.py
@@ -1,3 +1,12 @@
 def open_and_read_file(filename):
     try:
-        
+        file = open(filename)
+        content = file.read()
+        print(content)
+        file.close()
+
+    except FileNotFoundError:
+        print("Sorry, file doesn't exit")
+
+
+open_and_read_file("custom_error.py")
